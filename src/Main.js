7dbdcd5ab2b007d3c993e8ada6c2305e5bb676af
小林　鈴夏 <kobayashisuzuka@kobayashisuzunatsunoMacBook-Pro.local>
2026-02-20 00:23:38 +0900

    モーダル内容作成

--- src/Main.js
+++ src/Main.js
@@ -40,18 +40,25 @@ const Main = () => {
               <tbody>
                 <tr>
                   <td>担当工程</td>
-                  <td>環境</td>
+                  <td>DB設計、インフラ構築、コーディング、保守運用、UI/UXデザイン</td>
                 </tr>
                 <tr>
-                  <td>***</td>
-                  <td>***</td>
+                  <td>環境</td>
+                  <td>React.js、Node.js、AWS（EC2,Route53,CloudWatch,CodePipeLineなど）、MySQL、Adobe XD</td>
                 </tr>
               </tbody>
             </table>
-            <hr />
+            <div className="border" />
             <div className="project_details">
-              ********
-              ********
+              <div className="sub_title">詳細</div>
+              <div>
+                元々Microsoft Accessで管理していた、社内用の顧客情報を閲覧・編集できるアプリケーションを作成しました。
+                MySQLにデータ移行を行い、旧システムには無かった契約情報や訪問・アプローチ履歴などを残す機能を追加しています。<br />
+                エンジニアとして初めて担当した案件であり、学習のためデザイン〜リリースまでほとんど1人で担当しました。
+                AWSのCloudWatchで稼働状況を監視しており、エラーやサーバー停止などを感知してチャットに通知するよう設定しています。<br />
+                現在も社内でカスタマーサポート部署を中心に利用いただいています。
+              </div>
+              <div className="dummy_image"></div>
             </div>
           </div>
         </div>
